ccbc8d42688efa341d261b68040fa9632a937895
Виктория <vivlyakovleva@edu.hse.ru>
2026-06-02 00:42:57 +0300

    Добавила методы repr и eq

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ class Ingredient:
     def __init__(self, name, quantity, unit):
         self.name = name
         self.quantity = quantity
+        self.unit = unit
 
     def __str__(self):
         return f"{self.name}: {self.quantity} {self.unit}" 
+    def __repr__(self):
+        return f"Ingredient('{self.name}', '{self.quantity}', '{self.unit}')"
+    def __eq__(self, other):
+        if other.name==self.name and other.unit==self.unit:
+            return True
+        return False
