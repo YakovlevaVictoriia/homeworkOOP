edeeccb1a94b4fddcc16bcaf072635be54e8878f
Виктория <vivlyakovleva@edu.hse.ru>
2026-06-02 21:53:53 +0300

    первый тест про ингредиент, о инициализации

--- test_recipes.py
+++ test_recipes.py
@@ -1,2 +1,9 @@
 import pytest
 from recipes import *
+
+class TestIngredient:
+    def test_ingredient(self):
+        ingredient = Ingredient("Молоко", 250.0, "мл")
+        assert ingredient.name == "Молоко"
+        assert ingredient.amount == 250.0
+        assert ingredient.unit == "мл"
