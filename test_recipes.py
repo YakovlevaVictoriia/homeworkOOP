3261a39dcce635575a839c04533749000a0107a2
Виктория <vivlyakovleva@edu.hse.ru>
2026-06-02 22:00:27 +0300

    проверка на неположительное количество - реализован тест

--- test_recipes.py
+++ test_recipes.py
@@ -7,3 +7,9 @@ class TestIngredient:
         assert ingredient.name == "Молоко"
         assert ingredient.amount == 250.0
         assert ingredient.unit == "мл"
+    
+    def test_quantity_negative_amount(self):
+        with pytest.raises(ValueError, match="Количество должно быть положительным"):
+            Ingredient("Молоко", -250.0, "мл")
+        with pytest.raises(ValueError, match="Количество должно быть положительным"):
+            Ingredient("Молоко", 0.0, "мл")
